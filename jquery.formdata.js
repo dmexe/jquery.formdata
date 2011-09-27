779dd2dfc2864177bf62464e80435f1c1d07bb61
Dmitry Galinsky <dima.exe@gmail.com>
2011-09-27 22:35:17 +0400

    added upload progress

--- jquery.formdata.js
+++ jquery.formdata.js
@@ -1,37 +1,106 @@
 (function($) {
-    $.formData = function(el, options) {
-      var init = function(elem) {
-        // flash stuff
-      };
+    $.formData = {
+      nativeSubmit: function(form) {
+        var options = this.formOptions(form);
+        var fd = new FormData(form);
+        var d = options.deferred;
 
-      var submit = function() {
-        var $elem = $(this);
-        var data = new FormData(this);
-        var url = $elem.attr("action");
-        var type = $elem.attr("method");
-        console.log($elem);
-        var xhr = new XMLHttpRequest();
-        xhr.open(type, url);
-        xhr.send(data);
-        return false;
-      };
+        var xhr_provider = function() {
+          var xhr = $.ajaxSettings.xhr();
+          if(xhr.upload) {
+            xhr.upload.addEventListener('progress', function(ev){
+              d.list.progress.fireWith(ev, [(ev.loaded / ev.total) * 100]);
+            }, false);
+          }
+          return xhr;
+        };
 
-      init(el);
-      $(el).submit(submit);
+        $.ajax({
+            url:options.url,
+            data:fd,
+            processData:false,
+            type:options.type,
+            xhr: xhr_provider,
+            xhrFields: {
+              setRequestHeader:['Content-Type', '']
+            }
+        }).success(function(){
+            d.list.success.fireWith(this, arguments);
+        }).error(function(){
+            d.list.error.fireWith(this, arguments);
+        }).complete(function(){
+            d.list.complete.fireWith(this, arguments);
+        });
+      },
+
+      formOptions: function(form) {
+        $form = $(form);
+        options = {
+          url: $form.attr("action"),
+          type: $form.attr("method"),
+          deferred: $form.data("jquery.formdata.callbacks")
+        };
+        var opt = $form.data("jquery.formdata.options");
+        if (typeof opt !== 'undefined')  {
+          $.extend(options, opt);
+        }
+        if (options.urlFormat) {
+          options.url = options.url + "." + options.urlFormat;
+        }
+        return options;
+      },
+
+      setupSubmit: function($el) {
+        if (typeof FormData !== 'undefined') {
+          var self = this;
+          $el.submit(function(ev){
+            ev.preventDefault();
+            self.nativeSubmit(this);
+          });
+        } else {
+          console.log("jquery.formdata: FormData not supported!");
+        }
+      },
+
+      Callbacks: function() {
+        this.list = {
+          success: $.Callbacks("memory"),
+          error: $.Callbacks("memory"),
+          complete: $.Callbacks("memory"),
+          progress: $.Callbacks("memory")
+        };
+        this.success = this.list.success.add;
+        this.error = this.list.error.add;
+        this.complete = this.list.complete.add;
+        this.progress = this.list.progress.add;
+        return this;
+      },
+
+      init: function(el, options) {
+        $el = $(el);
+        callbacks = $el.data("jquery.formdata.callbacks");
+        if (typeof callbacks === 'undefined') {
+          callbacks = new this.Callbacks();
+          $el.data("jquery.formdata.callbacks", callbacks);
+          this.setupSubmit($el);
+        }
+        if (typeof options === 'object') {
+          $(el).data("jquery.formdata.options", options);
+        }
+        return callbacks;
+      },
+
+      defaultOptions: {}
     };
 
-    $.formData.defaultOptions = {};
-
     $.fn.formData = function(options) {
-      return this.each(function() {
-          ($.formData(this, options));
-      });
+      return ($.formData.init(this.get(0), options));
     };
 })(jQuery);
 
 /*
 $(document).ready(function(){
     var f = $("form");
-    f.formData();
+    f.formData().success(..).error(...).complete(...).progress(...);
 });
 */
